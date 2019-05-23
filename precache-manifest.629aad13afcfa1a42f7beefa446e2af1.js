90811a573d27b7a4540cad461f4eae7f6715f3aa
sang5052003 <sang5052003@naver.com>
2019-05-23 13:06:02 +0900

    Updates

---
+++ precache-manifest.629aad13afcfa1a42f7beefa446e2af1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "285093e552b5d104587c2804c990ac96",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "56c7f49b6a5afba08f77",
+    "url": "/movie_app/static/css/main.731b0bf4.chunk.css"
+  },
+  {
+    "revision": "fe232577e291977f94fb",
+    "url": "/movie_app/static/js/2.e936a4d1.chunk.js"
+  },
+  {
+    "revision": "56c7f49b6a5afba08f77",
+    "url": "/movie_app/static/js/main.2cdbec58.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
